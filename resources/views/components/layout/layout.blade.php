6edc9bc973ffbf75410987f273f070f174c052ea
Palinhas <carlosserranobicho@gmail.com>
2026-02-28 12:58:42 +0000

    27 - Add Alpine.js for enhanced interactivity and update Vite configuration

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Idea</title>
-    @vite('resources/css/app.css')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="bg-background text-foreground">
@@ -14,7 +14,11 @@
         {{ $slot }}
     </main>
    @session('success')
-        <div class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg ">
+        <div x-data="{ show: true }"
+             x-init="setTimeout(() => show = false, 3000)"
+             x-show="show"
+             x-transition.opacity.duration.300ms
+            class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg ">
             {{ $value }}
         </div>
     @endsession
